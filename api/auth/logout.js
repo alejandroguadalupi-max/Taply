4e5feed21c8715f439cb9a0147d6ae53abb4234c
Alejandro <alejandroguadalupi@gmail.com>
2025-08-30 21:06:36 +0100

    a

--- api/auth/logout.js
+++
@@ -1,7 +0,0 @@
-// /api/auth/logout.js
-import { clearCookie, json } from '../_utils.js';
-
-export default async function handler(req, res) {
-  clearCookie(res, 'taply_session');
-  return json(res, 200, { ok:true });
-}
--- api/auth/me.js
+++
@@ -1,8 +0,0 @@
-// /api/auth/me.js
-import { getSessionUser, json } from '../_utils.js';
-
-export default async function handler(req, res) {
-  const user = getSessionUser(req);
-  if (!user) return json(res, 401, { ok:false });
-  return json(res, 200, { ok:true, user });
-}
